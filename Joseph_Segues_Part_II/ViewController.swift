007a064e635c0a8fe34ff291f904dea4a17e8250
allan_joseph <allanjoseph70@gmail.com>
2021-04-13 12:20:11 -0400

    Initial Commit

---
+++ Joseph_Segues_Part_II/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joseph_Segues_Part_II
+//
+//  Created by Allan Joseph on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
